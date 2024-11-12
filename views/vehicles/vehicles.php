5bbe3022785ab86b9f43492e87c3b24f69d32244
pgm-casverheye <cas.verheye@student.arteveldehs.be>
2024-11-12 13:40:08 +0100

    small update

--- views/vehicles/vehicles.php
+++ views/vehicles/vehicles.php
@@ -10,11 +10,6 @@
             <input type="text" id="searchBar" class="max-w-xs border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500" placeholder="Search by Make or Model...">
             <button id="searchButton" class="bg-blue-500 text-white font-semibold p-2 rounded-md hover:bg-blue-600 transition duration-200">Search</button>
         </div>
-
-        <div class="mb-4 flex items-center">
-            <input type="text" id="licensePlateSearchBar" class="max-w-xs border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500" placeholder="Search by License Plate...">
-            <button id="licensePlateSearchButton" class="bg-blue-500 text-white font-semibold p-2 rounded-md hover:bg-blue-600 transition duration-200">Search</button>
-        </div>
     </div>
 
 
